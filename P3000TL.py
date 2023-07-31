6aba9dd1f2be943d59fc603b73148e989c5ea559
admin <admin@Serjs-MacBook.local>
2023-08-01 03:13:17 +0700

    Upgraded the function description of 'birthday_set' and 'birthday_get'. Added a handler to a 'birthday_get' function. Okay, admit that I have really overstemated my skills in bot creation. 5 minutes of expected work bloated to 5 hours. It was fun to do, tho.

--- P3000TL.py
+++ P3000TL.py
@@ -49,20 +49,55 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def birthday_set(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
+    Set the user's birthday date.
+
     This function takes up to two optional arguments.
 
-    Using it without any argument will add THIS user as the
-    birthday person and set their birthday day to TODAY.
+    Using it without any argument will
+    add THIS user as the birthday person and
+    set their birthday day to TODAY.
 
-    Using it with one argument (person) will add THAT user as the
-    birthday person and set their birthday day to TODAY.
+    Using it with one argument (person) will
+    add THAT user as the birthday person and
+    set their birthday day to TODAY.
 
-    Using it with two arguments (person, date) will add THAT user
-    as the birthday person and set their birthday day to that DATE.
+    Using it with two arguments (person, date) will
+    add THAT user as the birthday person and
+    set their birthday day to that DATE.
+
+    if anything goes wrong, send a message with "n/a"
 
     This function returns nothing.
     This function doesn't raise any errors.
     """
+
+    # WIP
+    pass
+
+
+async def birthday_get(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    Peek the user's birthday date.
+
+    This function takes up to one optional argument.
+
+    Using it without any argument will
+    send a message with THIS user's birthday date.
+
+    Using it with one argument (person) will
+    send a message with THAT user's birthday date.
+
+    Using it with one argument (date) will
+    send a message with all users with matching birthday DATE.
+
+    If anything goes wrong, send a message with "n/a".
+
+    This function returns nothing.
+    This function doesn't raise any errors.
+    """
+
+    # WIP
+    pass
 
 
 if __name__ == '__main__':
@@ -70,12 +105,14 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(getenv('TG_BOT_TOKEN')).build()
 
     # setting up the commands
-    birthday_set_handler = CommandHandler('give_birth', birthday_set)
+    birthday_set_handler = CommandHandler('ya_rodilsa', birthday_set)
+    birthday_get_handler = CommandHandler('kogda_dr', birthday_get)
     start_handler = CommandHandler('start', start)
 
     # applying said commands for the bot to recognize them
     # POSITION MATTERS: the bot will check them in order of appearence
     application.add_handler(start_handler)
+    application.add_handler(birthday_get_handler)
     application.add_handler(birthday_set_handler)
 
     # asking the server for anything new every couple of seconds
