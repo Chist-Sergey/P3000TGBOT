05bc9fac952118910d05c4143baf4936a8c75654
admin <admin@Serjs-MacBook.local>
2023-09-13 23:39:14 +0700

    Created the 'text_responses.py' for store the bot's text responses and for easier bot maintance.

---
+++ text_responses.py
@@ -0,0 +1,18 @@
+# Easy access to any bot text!
+# I hope it will be replaced by far more
+# superior message 'reactions'.
+
+def greeting() -> str:
+    return 'Опять работать.'
+
+def db_write_success() -> str:
+    return 'Запомнил.'
+
+def search_name_fail() -> str:
+    return 'Нет такого.'
+
+def search_date_success() -> str:
+    return 'В этот день празднуют др:\n'
+
+def search_date_fail() -> str:
+    return 'В этот день именинников нет.\n'
