de460d381445462ed8aa05daba2826a1d852132f
admin <admin@Serjs-MacBook.local>
2023-12-17 23:52:42 +0700

    Created a manager file for button text in inline keyboards 'button_text.py'.

---
+++ button_text.py
@@ -0,0 +1,14 @@
+def key_add():
+    return 'Прибавить на 2',
+
+
+def key_substract():
+    return 'Убавить на 1',
+
+
+def key_continue():
+    return 'Продолжить',
+
+
+def key_abort():
+    return 'Отмена',
