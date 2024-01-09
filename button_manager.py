6e170979efe9e5a26540977a6de4ae159cd9c9f9
Chist-Sergey <sergeychistov4088@gmail.com>
2024-01-09 23:09:23 +0700

    Created a new set of keys class named 'DaysButton' in a 'button_manager.py' file.

--- button_manager.py
+++ button_manager.py
@@ -74,3 +74,147 @@ class MonthsButton:
 
     def dec():
         return ('(12) Декабрь', '15',)
+
+
+class DaysButton:
+    def one_to_six():
+        return ('1-6', '0')
+
+
+    def seven_to_twelve():
+        return ('7-12', '1')
+
+
+    def thirteen_to_eighteen():
+        return ('13-18', '2')
+
+
+    def nineteen_to_twentyfour():
+        return ('19-24', '3')
+
+
+    def twentyfive_to_thirtyone():
+        return ('25-31', '4')
+
+
+    def one():
+        return ('1', '5')
+
+
+    def two():
+        return ('2', '6')
+
+
+    def three():
+        return ('3', '7')
+
+
+    def four():
+        return ('4', '8')
+
+
+    def five():
+        return ('5', '9')
+
+
+    def sex():
+        return ('6', '10')
+
+
+    def seven():
+        return ('7', '11')
+
+
+    def eight():
+        return ('8', '12')
+
+
+    def nine():
+        return ('9', '13')
+
+
+    def ten():
+        return ('10', '14')
+
+
+    def eleven():
+        return ('11', '15')
+
+
+    def twelve():
+        return ('12', '16')
+
+
+    def thirteen():
+        return ('13', '17')
+
+
+    def fourteen():
+        return ('14', '18')
+
+
+    def fifteen():
+        return ('15', '19')
+
+
+    def sixteen():
+        return ('16', '20')
+
+
+    def seventeen():
+        return ('17', '21')
+
+    def eighteen():
+        return ('18', '22')
+
+
+    def nineteen():
+        return ('19', '23')
+
+
+    def twenty():
+        return ('20', '24')
+
+
+    def twentyone():
+        return ('21', '25')
+
+
+    def twentytwo():
+        return ('22', '26')
+
+
+    def twentythree():
+        return ('23', '27')
+
+
+    def twentyfour():
+        return ('24', '28')
+
+
+    def twentyfive():
+        return ('25', '29')
+
+
+    def twentysix():
+        return ('26', '30')
+
+
+    def twentyseven():
+        return ('27', '32')
+
+
+    def twentyeight():
+        return ('28', '32')
+
+
+    def twentynine():
+        return ('29', '33')
+
+
+    def thirty():
+        return ('30', '34')
+
+
+    def thirtyone():
+        return ('31', '35')
